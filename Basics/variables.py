350d2f413e0fe41fd87a51e4c439a6969f2fa25e
Shreeram N S <nsshreeram@gmail.com>
2024-04-28 00:57:19 +0530

    Day 1
    Learnt about print function and variables in python

---
+++ Basics/variables.py
@@ -0,0 +1,19 @@
+name = "Shreeram"
+age = 20
+cgpa = 9.2
+
+age2 = age
+Studying = true
+
+print("name:", name)
+print("age:", age)
+print("cgpa:",cgpa)
+print("age2:",age2)
+
+print(type(name))
+print(type(age))
+print(type(cgpa))
+
+#name is variable or memory location which stores string value ,string - is collection of letters or numbers or symbols enclosed  in double quotes
+# age is variable or memory locacgpation which stores integer value
+# age is variable or memory location which stores floating value = decimal value
